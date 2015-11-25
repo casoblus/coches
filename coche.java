48c9dc365cec87c6be1eec0e246e2eae0d3cf908
casoblus <casobrinolusquinos@gmail.com>
2015-11-25 22:20:13 +0100

    atributo estático numero de coches

--- coche.java
+++ coche.java
@@ -2,6 +2,7 @@ class coche
 {
 	private String matricula, marca, modelo;
 	private float km, combustible, consumer;
+	static int n_coches = 0;
 	
 	public void ALTA( String mat, String mar, String mod, float cons )
 	{
@@ -9,6 +10,7 @@ class coche
 		marca = mar;
 		modelo = mod;
 		consumer = cons;
+		INCREMENTA_COCHES();
 	}
 
 	public void VER_DATOS()
@@ -18,6 +20,7 @@ class coche
 		System.out.println(modelo);
 		System.out.println(km);
 		System.out.println(combustible);
+		System.out.println( "N coches: " + n_coches );
 	}
 	
 	public float VER_KM()
@@ -39,10 +42,10 @@ class coche
 			km += dist;
 			if( combustible <= 5 )
 			{
-				this.RESERVA();
+				RESERVA();
 			}
 		} else { 
-			this.RESERVA();
+			RESERVA();
 		}
 	}
 
@@ -53,5 +56,10 @@ class coche
 		System.out.println("REPOSTE CANTO ANTES");
 		System.out.println("REPOSTE CANTO ANTES");
 		System.out.println("REPOSTE CANTO ANTES");
+	}
+
+	private void INCREMENTA_COCHES()
+	{
+		n_coches++;
 	}
 }
